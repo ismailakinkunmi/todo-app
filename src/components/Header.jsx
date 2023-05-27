1dc87f49a7d6b0eab37ffc2c586659d2aa1e2ddf
Adam Jamiu <adismail4@gmail.com>
2023-05-27 17:06:20 +0100

    Add navbar components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-//import React from "react";
+import Navbar from "@/components/Navbar";
 
 const Header = () => {
   return (
     <header>
+      <Navbar />
       <h1>todos</h1>
       <p>Items will persist in the browser local storage.</p>
     </header>
