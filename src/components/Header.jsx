b12d2252e77d44e29a2f1855c4e586e99de6f437
Adam Jamiu <adismail4@gmail.com>
2023-05-30 16:49:08 +0100

    Add header features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import Navbar from "@/components/Navbar";
+//import Navbar from "@/components/Navbar";
+import styles from "@/styles/Header.module.css";
 
 const Header = () => {
+  const headStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+    color: "#aeadad",
+    textAlign: "center",
+  };
   return (
-    <header>
-      <Navbar />
+    <header style={headStyle} className={styles.Header}>
       <h1>todos</h1>
       <p>Items will persist in the browser local storage.</p>
     </header>
